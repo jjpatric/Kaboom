d19d85f620fffabc43a9a8aabe902cd3b73b9bc6
Joshua Patrick <32472041+jjpatric@users.noreply.github.com>
2017-11-28 14:02:13 -0700

    helped astetic

--- Final.cpp
+++ Final.cpp
@@ -39,93 +39,20 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 
 
-
-
 void setup(){
-init();
-Serial.begin(9600);
+	init();
+	Serial.begin(9600);
 	tft.begin();
 	pinMode(JOY_SEL, INPUT);
 	tft.setRotation(3);
 	tft.setTextWrap(false);
-tft.fillScreen(GREEN);
-
-tft.fillRect(0, 0, DISPLAY_WIDTH, 48, GREY);
-
+	tft.fillScreen(GREEN);
+	tft.fillRect(0, 0, DISPLAY_WIDTH, 48, GREY);
 }
 
-//function for the main menu. using the joystick, when 1 player is selected,
-//returns 0. When Head to head is selected, returns 1
-bool startmenu(){
-bool value = 0;
-tft.setCursor(90, 50);
-tft.setTextColor(RED);
-tft.setTextSize(3);
-tft.setTextWrap(false);
-tft.print("KABOOM!!");
-
-
-//initally highlights 1 player
-tft.setCursor(90, 90);
-tft.setTextColor(WHITE, MAGENTA);
-tft.setTextSize(2);
-tft.setTextWrap(false);
-tft.print("1 Player Mode");
-
-tft.setCursor(90, 120);
-tft.setTextColor(WHITE, GREEN);
-tft.setTextSize(2);
-tft.setTextWrap(false);
-tft.print("Head To Head!!");
-
-while (true){
-int Yvalue = analogRead(JOY_VERT_ANALOG);
-Serial.println(Yvalue);
-
-if (Yvalue > JOY_CENTRE + JOY_DEADZONE) {
-	tft.setCursor(90, 90);
-	tft.setTextColor(WHITE, GREEN);
-	tft.setTextSize(2);
-	tft.setTextWrap(false);
-	tft.print("1 Player Mode");
-
-	tft.setCursor(90, 120);
-	tft.setTextColor(WHITE, MAGENTA);
-	tft.setTextSize(2);
-	tft.setTextWrap(false);
-	tft.print("Head To Head!!");
-
-value = 1;
-}
-
-if (Yvalue < JOY_CENTRE - JOY_DEADZONE) {
-
-	tft.setCursor(90, 90);
-	tft.setTextColor(WHITE, MAGENTA);
-	tft.setTextSize(2);
-	tft.setTextWrap(false);
-	tft.print("1 Player Mode");
-
-	tft.setCursor(90, 120);
-	tft.setTextColor(WHITE, GREEN);
-	tft.setTextSize(2);
-	tft.setTextWrap(false);
-	tft.print("Head To Head!!");
-	value = 0;
-}
-
-
-
-
-}
-return 1;
-}
 int main(){
-setup();
-
-bool startval = startmenu();
-
-
+	setup();
+	bool startval = startmenu();
 
 	return 0;
 }
