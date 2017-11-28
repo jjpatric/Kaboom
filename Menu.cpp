d09d117d89ebd169ad0de6f9bbd94f62ebb8965c
Joshua Patrick <32472041+jjpatric@users.noreply.github.com>
2017-11-28 14:04:28 -0700

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -5,49 +5,15 @@
 //function for the main menu. using the joystick, when 1 player is selected,
 //returns 0. When Head to head is selected, returns 1
 bool startmenu(){
-bool value = 0;
-tft.setCursor(90, 50);
-tft.setTextColor(RED);
-tft.setTextSize(3);
-tft.setTextWrap(false);
-tft.print("KABOOM!!");
-
-
-//initally highlights 1 player
-tft.setCursor(90, 90);
-tft.setTextColor(WHITE, MAGENTA);
-tft.setTextSize(2);
-tft.setTextWrap(false);
-tft.print("1 Player Mode");
-
-tft.setCursor(90, 120);
-tft.setTextColor(WHITE, GREEN);
-tft.setTextSize(2);
-tft.setTextWrap(false);
-tft.print("Head To Head!!");
-
-while (true){
-int Yvalue = analogRead(JOY_VERT_ANALOG);
-Serial.println(Yvalue);
-
-if (Yvalue > JOY_CENTRE + JOY_DEADZONE) {
-	tft.setCursor(90, 90);
-	tft.setTextColor(WHITE, GREEN);
-	tft.setTextSize(2);
+	bool value = 0;
+	tft.setCursor(90, 50);
+	tft.setTextColor(RED);
+	tft.setTextSize(3);
 	tft.setTextWrap(false);
-	tft.print("1 Player Mode");
+	tft.print("KABOOM!!");
 
-	tft.setCursor(90, 120);
-	tft.setTextColor(WHITE, MAGENTA);
-	tft.setTextSize(2);
-	tft.setTextWrap(false);
-	tft.print("Head To Head!!");
 
-value = 1;
-}
-
-if (Yvalue < JOY_CENTRE - JOY_DEADZONE) {
-
+	//initally highlights 1 player
 	tft.setCursor(90, 90);
 	tft.setTextColor(WHITE, MAGENTA);
 	tft.setTextSize(2);
@@ -59,5 +25,42 @@ if (Yvalue < JOY_CENTRE - JOY_DEADZONE) {
 	tft.setTextSize(2);
 	tft.setTextWrap(false);
 	tft.print("Head To Head!!");
-	value = 0;
+
+	while (true){
+		int Yvalue = analogRead(JOY_VERT_ANALOG);
+		Serial.println(Yvalue);
+
+		if (Yvalue > JOY_CENTRE + JOY_DEADZONE) {
+			tft.setCursor(90, 90);
+			tft.setTextColor(WHITE, GREEN);
+			tft.setTextSize(2);
+			tft.setTextWrap(false);
+			tft.print("1 Player Mode");
+
+			tft.setCursor(90, 120);
+			tft.setTextColor(WHITE, MAGENTA);
+			tft.setTextSize(2);
+			tft.setTextWrap(false);
+			tft.print("Head To Head!!");
+			value = 1;
+		}
+
+		if (Yvalue < JOY_CENTRE - JOY_DEADZONE) {
+
+			tft.setCursor(90, 90);
+			tft.setTextColor(WHITE, MAGENTA);
+			tft.setTextSize(2);
+			tft.setTextWrap(false);
+			tft.print("1 Player Mode");
+
+			tft.setCursor(90, 120);
+			tft.setTextColor(WHITE, GREEN);
+			tft.setTextSize(2);
+			tft.setTextWrap(false);
+			tft.print("Head To Head!!");
+			value = 0;
+		}
+
+	}
+	return 1;
 }
